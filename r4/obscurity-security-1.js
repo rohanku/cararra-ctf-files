76ebb69e0ceb0a45da3b1fc8f96f64135629fbb1
Rohan Kumar <rohan@heberway.com>
2021-07-03 13:11:12 -0700

    fix

--- r4/obscurity-security-1.js
+++ r4/obscurity-security-1.js
@@ -2,22 +2,23 @@ const WebSocket = require("ws");
 const { exec } = require("child_process");
 
 function run(ws) {
-  const process = exec('./r4/obscurity-security-1', []);
+  const process = exec('./r4/obscurity-security-2', []);
   process.stdout.on('data', (data) => {
-    if (ws.readyState === WebSocket.CLOSED) return;
+    if (ws.readyState !== WebSocket.OPEN) return;
     ws.send(data);
   });
   process.stderr.on('data', (data) => {
-    if (ws.readyState === WebSocket.CLOSED) return;
+    if (ws.readyState !== WebSocket.OPEN) return;
     ws.send(data);
   });
-  process.on('close', (data) => {
-    setTimeout(() => { ws?.close }, 1000);
+  process.on('exit', (data) => {
+    ws.close();
   });
   ws.on("message", function incoming(message) {
-    if (ws.readyState === WebSocket.CLOSED) return;
+    if (ws.readyState !== WebSocket.OPEN) return;
     process.stdin.write(message);
   });
 }
 
 module.exports = { run };
+
