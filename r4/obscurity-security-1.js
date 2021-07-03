ddab1bf4f2241a972976f53706a328d2d7889f6d
Rohan Kumar <rohan@heberway.com>
2021-07-03 12:28:28 -0700

    finish

--- r4/obscurity-security-1.js
+++ r4/obscurity-security-1.js
@@ -2,12 +2,15 @@ const WebSocket = require("ws");
 const { exec } = require("child_process");
 
 function run(ws) {
-  const process = exec('./r4/obscurity-security-1', []);
+  const process = exec('./r4/obscurity-security-2', []);
   process.stdout.on('data', (data) => {
     ws.send(data);
   });
   process.stderr.on('data', (data) => {
     ws.send(data);
+  });
+  process.stderr.on('end', (data) => {
+    ws.close();
   });
   ws.on("message", function incoming(message) {
     process.stdin.write(message);
