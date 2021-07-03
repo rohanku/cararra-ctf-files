75c0265733c3cbe37366803b94f62abeaa58b6bc
Rohan Kumar <rohan@heberway.com>
2021-07-03 12:56:50 -0700

    epic

--- r4/obscurity-security-2.js
+++ r4/obscurity-security-2.js
@@ -4,15 +4,18 @@ const { exec } = require("child_process");
 function run(ws) {
   const process = exec('./r4/obscurity-security-2', []);
   process.stdout.on('data', (data) => {
+    if (ws.readyState === WebSocket.CLOSED) return;
     ws.send(data);
   });
   process.stderr.on('data', (data) => {
+    if (ws.readyState === WebSocket.CLOSED) return;
     ws.send(data);
   });
   process.on('close', (data) => {
-    setTimeout(ws.close, 1000);
+    setTimeout(() => { ws?.close }, 1000);
   });
   ws.on("message", function incoming(message) {
+    if (ws.readyState === WebSocket.CLOSED) return;
     process.stdin.write(message);
   });
 }
